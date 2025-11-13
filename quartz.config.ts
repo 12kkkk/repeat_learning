4ad820fc5ee0c6231c92dae4c548ef8d7fe3442e
12kkk <3132702138@qq.com>
2025-11-13 19:40:15 +0800

    update

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "12kkkk.github.io/repeat_learning/",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian","docs/plugins/CustomOrgImages.md"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
