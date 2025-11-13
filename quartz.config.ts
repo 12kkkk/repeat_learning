1f4d18a4faf8210e4089727d1ade7b8431c423f3
12kkk <3132702138@qq.com>
2025-11-13 19:07:03 +0800

    update

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "飞鸿踏雪的知识仓库",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
